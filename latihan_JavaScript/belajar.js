236a4f6fdc0caccbd20b076f3b2932206a247b31
bagasghifari <e42242118@student.polije.ac.id>
2025-09-10 15:08:52 +0700

    latihan_bisdig

---
+++ latihan_JavaScript/belajar.js
@@ -0,0 +1,6 @@
+
+    console.log("Ini dari file index.js!");
+
+    const newElement = document.createElement('p');
+    newElement.textContent = "Ini dibuat oleh JavaScript!";
+    document.body.appendChild(newElement);
